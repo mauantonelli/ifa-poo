98b75e5b6ef9b17bd29022416ad6fafa1e430771
mauantonelli <pablojuanang@gmail.com>
2024-11-22 15:35:41 -0300

    connection factory

--- src/main/java/adapters/dao/ManutencaoDAOimpl.java
+++ src/main/java/adapters/dao/ManutencaoDAOimpl.java
@@ -1,13 +1,24 @@
 package adapters.dao;
 
+import domain.entity.Manutencao;
 import domain.entity.Mecanico;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
 public class ManutencaoDAOimpl implements MecanicoDAO {
+
+
     @Override
-    public int save(Mecanico type) {
+    public int save(Manutencao manutencao) {
+        String sql = "INSERT INTO manutencao (id, data, custo, veiculo, mecanico) vales (?,?,?,?,?)";
+        int generateId=0;
+        try(PreparedStatement stmt = )
+
+
+
+
         return 0;
     }
 
