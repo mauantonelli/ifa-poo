b89845f3a6cafc86c5305318af73f56b132f69f7
mauantonelli <pablojuanang@gmail.com>
2024-11-22 15:43:56 -0300

    methods impl manutencao

--- src/main/java/adapters/dao/ManutencaoDAOimpl.java
+++ src/main/java/adapters/dao/ManutencaoDAOimpl.java
@@ -1,36 +1,51 @@
 package adapters.dao;
 
+import adapters.ConnectionFactory;
 import domain.entity.Manutencao;
 import domain.entity.Mecanico;
+import domain.entity.Veiculo;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
-public class ManutencaoDAOimpl implements MecanicoDAO {
+public class ManutencaoDAOimpl implements ManutencaoDAO {
 
 
     @Override
     public int save(Manutencao manutencao) {
-        String sql = "INSERT INTO manutencao (id, data, custo, veiculo, mecanico) vales (?,?,?,?,?)";
-        int generateId=0;
-        try(PreparedStatement stmt = )
+        String sql = "INSERT INTO manutencao (id, data, custo) values (?,?,?)";
+        int generatedId = 0;
+        try (PreparedStatement stmt = ConnectionFactory.createStatement(sql)) {
+            stmt.setInt(1, manutencao.getId());
+            stmt.setString(2, manutencao.getData());
+            stmt.setDouble(2, manutencao.getCusto());
+            stmt.executeUpdate();
 
+            ResultSet generatedKeys = stmt.getGeneratedKeys();
+            if (generatedKeys.next())
+                generatedId = generatedKeys.getInt(1);
 
-
-
-        return 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return generatedId;
     }
 
     @Override
-    public Optional<Mecanico> findById(int id) {
+    public Optional<Manutencao> findById(int id) {
         return Optional.empty();
     }
 
     @Override
-    public List<Mecanico> findAll() {
+    public List<Manutencao> findAll() {
         return null;
     }
 
-
+    @Override
+    public List<Manutencao> obterManutencaoPorVeiculo(Veiculo veiculo) {
+        return null;
+    }
 }
