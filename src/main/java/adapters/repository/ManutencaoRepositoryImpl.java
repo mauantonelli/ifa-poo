1d721aa89974dd36a272c658d03e477296d77834
mauantonelli <pablojuanang@gmail.com>
2024-11-22 12:55:39 -0300

    methods impl repository adapters

--- src/main/java/adapters/repository/ManutencaoRepositoryImpl.java
+++ src/main/java/adapters/repository/ManutencaoRepositoryImpl.java
@@ -1,6 +1,40 @@
 package adapters.repository;
 
-public class ManutencaoRepositoryImpl {
+import adapters.dao.ManutencaoDAO;
+import domain.entity.Manutencao;
+import domain.entity.Veiculo;
+import domain.repository.ManutencaoRepository;
 
-    // Associacao com o DAO, similar ao UseCase
+import java.util.List;
+import java.util.Optional;
+
+public class ManutencaoRepositoryImpl implements ManutencaoRepository {
+
+  private final ManutencaoDAO dao;
+
+    public ManutencaoRepositoryImpl(ManutencaoDAO dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public List<Manutencao> obterManutencaoPorVeiculo(Veiculo veiculo) {
+        return null;
+    }
+
+    @Override
+    public void save(Manutencao manutencao) {
+        int id= dao.save(manutencao);
+        manutencao.setId(id);
+
+    }
+
+    @Override
+    public Optional<Manutencao> findById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Manutencao> findAll() {
+        return null;
+    }
 }
