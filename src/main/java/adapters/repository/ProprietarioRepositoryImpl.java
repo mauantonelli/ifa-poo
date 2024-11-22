e770e0a46590fb0ab44702be4e4e8077b0fc7230
mauantonelli <pablojuanang@gmail.com>
2024-11-22 13:50:20 -0300

    up all methods impl

--- src/main/java/adapters/repository/ProprietarioRepositoryImpl.java
+++ src/main/java/adapters/repository/ProprietarioRepositoryImpl.java
@@ -1,4 +1,33 @@
 package adapters.repository;
 
-public class ProprietarioRepositoryImpl {
+import adapters.dao.ProprietarioDAO;
+import domain.entity.Proprietario;
+import domain.repository.ProprietarioRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProprietarioRepositoryImpl implements ProprietarioRepository {
+    private final ProprietarioDAO dao;
+
+    public ProprietarioRepositoryImpl(ProprietarioDAO dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public void save(Proprietario proprietario) {
+        int id=dao.save(proprietario);
+        proprietario.setId(id);
+
+    }
+
+    @Override
+    public Optional<Proprietario> findById(int id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public List<Proprietario> findAll() {
+        return dao.findAll();
+    }
 }
--- src/main/java/adapters/repository/VeiculoRepositoryImpl.java
+++ src/main/java/adapters/repository/VeiculoRepositoryImpl.java
@@ -1,4 +1,46 @@
 package adapters.repository;
 
-public class VeiculoRepositoryImpl {
+import adapters.dao.VeiculoDAO;
+import domain.entity.Proprietario;
+import domain.entity.Veiculo;
+import domain.repository.VeiculoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class VeiculoRepositoryImpl implements VeiculoRepository {
+    private final VeiculoDAO dao;
+
+    @Override
+    public void save(Veiculo veiculo) {
+        int id=dao.save(veiculo);
+        veiculo.setId(id);
+
+    }
+
+    @Override
+    public Optional<Veiculo> findById(int id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public List<Veiculo> findAll() {
+        return findAll();
+    }
+
+    @Override
+    public List<Veiculo> obterVeiculoPorProprietario(Proprietario proprietario) {
+        return dao.obterVeiculoPorProprietario(proprietario);
+    }
+
+    @Override
+    public List<Veiculo> obterVeiculoPorPlaca(String placa) {
+        return dao.obterVeiculoPorPlaca(placa);
+    }
+
+    public VeiculoRepositoryImpl(VeiculoDAO dao) {
+        this.dao = dao;
+
+
+    }
 }
