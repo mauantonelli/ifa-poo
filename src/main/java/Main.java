2bc2a65d08867a160972a697e006a881e7767c0a
mauantonelli <pablojuanang@gmail.com>
2024-11-25 12:59:06 -0300

    save test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,20 @@
+import adapters.dao.MecanicoDAO;
+import adapters.dao.MecanicoDAOimpl;
+import adapters.repository.MecanicoRepositoryImpl;
+import domain.entity.Mecanico;
+import domain.repository.MecanicoRepository;
+import domain.usecase.MecanicoUseCase;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        MecanicoDAO mecanicoDAO = new MecanicoDAOimpl();
+        MecanicoRepository mecanicoRepository = new MecanicoRepositoryImpl(mecanicoDAO);
+        MecanicoUseCase mecanicoUseCase = new MecanicoUseCase(mecanicoRepository);
+
+        Mecanico mecanico1 = new Mecanico(1, "Maurício", 1200);
+        mecanicoUseCase.save(mecanico1);
+
+    }
 }
